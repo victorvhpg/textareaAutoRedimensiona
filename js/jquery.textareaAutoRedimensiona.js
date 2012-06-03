6309fdd6a02d83b94e6d95ba2bbad0339cd317f1
Victor Hugo <victorvhpg@gmail.com>
2012-06-03 19:39:13 -0300

    ;)

--- js/jquery.textareaAutoRedimensiona.js
+++ js/jquery.textareaAutoRedimensiona.js
@@ -5,37 +5,36 @@
  * https://github.com/victorvhpg/textareaAutoRedimensiona
  * 
  * auto redimensiona altura de um textarea
- */
-(function($) {
-    $.fn.textareaAutoRedimensiona = function() {
-        return this.each(function() {
-            var cssQueAlteraTamanho = ['font-size','font-style', 'font-weight', 'font-family','line-height', 'text-transform', 'letter-spacing'];
-			var css = {};
-			//pega o css que pode alterar as dimensoes do texto
-			for(var i =0 ; i <  cssQueAlteraTamanho.length; i++){
-				css[cssQueAlteraTamanho[i]] = $(this).css(cssQueAlteraTamanho[i]);
-			}           
-		   $(this).on("keypress keydown" , function(){
+ */ (function ($) {
+    $.fn.textareaAutoRedimensiona = function () {
+        return this.each(function () {
+            var cssQueAlteraTamanho = ['font-size', 'font-style', 'font-weight', 'font-family', 'line-height', 'text-transform', 'letter-spacing'];
+            var css = {};
+            //pega o css que pode alterar as dimensoes do texto
+            for (var i = 0; i < cssQueAlteraTamanho.length; i++) {
+                css[cssQueAlteraTamanho[i]] = $(this).css(cssQueAlteraTamanho[i]);
+            }
+            $(this).on("keypress keydown", function () {
                 var texto = $(this).val();
                 var largura = $(this).width();
-                var $temp = $("<div />" , {
-                    "css" :$.extend(css , {
-                        "position" : "absolute" ,
-                        "left" : -999999999999,
-                        "top" : -99999,
-                        "width" : largura + "px",
-                        "float" : "left",
-                        "background-color" : "#00ff00",
+                var $temp = $("<div />", {
+                    "css": $.extend(css, {
+                        "position": "absolute",
+                        "left": -999999999999,
+                        "top": -99999,
+                        "width": largura + "px",
+                        "float": "left",
+                        "background-color": "#00ff00",
                         "word-wrap": "break-word"
-                    }) ,
-                    "html" : texto.replace(/ /gim,"&nbsp;" ).replace(/\n/gim,"<br />" )
-                
+                    }),
+                    "html": texto.replace(/ /gim, "&nbsp;").replace(/\n/gim, "<br />")
+
                 }).appendTo("body");
                 var fs = parseInt($temp.css("font-size"));
-                var altura= $temp.height()+fs;
-                $(this).stop(true,true).animate({
-                    "height" : altura+"px"
-                },600);
+                var altura = $temp.height() + fs;
+                $(this).stop(true, true).animate({
+                    "height": altura + "px"
+                }, 600);
                 $temp.remove();
             });
         }); //each
