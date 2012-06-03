0053d234f89371e113d035b11857a90bdd5bdadf
Victor Hugo <victorvhpg@gmail.com>
2012-06-03 19:12:42 -0300

    auto redimensiona altura de um textarea

---
+++ js/jquery.textareaAutoRedimensiona.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ * @victorvhpg
+ * 03/06/2012
+ * https://github.com/victorvhpg/textareaAutoRedimensiona
+ * 
+ * auto redimensiona altura de um textarea
+ */
+(function($) {
+    $.fn.textareaAutoRedimensiona = function() {
+        return this.each(function() {
+            var cssQueAlteraTamanho = ['font-size','font-style', 'font-weight', 'font-family','line-height', 'text-transform', 'letter-spacing'];
+            $(this).on("keypress keydown" , function(){
+                var i;
+                var texto = $(this).val();
+                var largura = $(this).width();
+                var css = {};
+                //pega o css que pode alterar as dimensoes do texto
+                for(i =0 ; i <  cssQueAlteraTamanho.length; i++){
+                    css[cssQueAlteraTamanho[i]] = $(this).css(cssQueAlteraTamanho[i]);
+                }
+                var $temp = $("<div />" , {
+                    "css" :$.extend(css , {
+                        "position" : "absolute" ,
+                        "left" : -999999999999,
+                        "top" : -99999,
+                        "width" : largura + "px",
+                        "float" : "left",
+                        "background-color" : "#00ff00",
+                        "word-wrap": "break-word"
+                    }) ,
+                    "html" : texto.replace(/ /gim,"&nbsp;" ).replace(/\n/gim,"<br />" )
+                
+                }).appendTo("body");
+                var fs = parseInt($temp.css("font-size"));
+                var altura= $temp.height()+fs;
+                $(this).stop(true,true).animate({
+                    "height" : altura+"px"
+                },600);
+                $temp.remove();
+            });
+        }); //each
+    }; //textareaAutoRedimensiona
+})(jQuery);
